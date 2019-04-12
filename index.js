b8966d87acf87b9a0ee524357db0a740498ade1b
cjbangert <cjbangert@DESKTOP-UMHAJ50.localdomain>
2019-04-12 01:21:39 -0400

    fixed search

--- index.js
+++ index.js
@@ -138,6 +138,10 @@ app.get('/api/quick',function(req,res){
 
 
 
-app.listen(3000, function() {
+/*app.listen(3000, function() {
     console.log('Listening on port 3000!');
+});
+*/
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Listening!');
 });
